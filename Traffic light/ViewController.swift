652a0c271d213f4ef91b6877d26c10fb4d09eb5a
Dmitry <borodulkin_d@mail.ru>
2023-03-09 15:42:48 +0300

    Created IBOutlets, IBAction, properties.

--- Traffic light/ViewController.swift
+++ Traffic light/ViewController.swift
@@ -7,12 +7,36 @@
 
 import UIKit
 
+enum CurrentLight {
+case red, yellow, green
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    private var currentLight = CurrentLight.red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.4
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startButton.layer.cornerRadius = 10
+        
+        redLight.alpha = lightIsOff
+        yellowLight.alpha = lightIsOff
+        greenLight.alpha = lightIsOff
+        
     }
+    
+    @IBAction func startButtonPressed() {
+        
+    }
+    
 
 
 }
